dee510df6f10b4af008ad76a68f0524ead5fbf3e
unknown <s.rudchyk@srudchyk-nb.vinnica.astoundcommerce.com>
2015-06-11 17:56:11 +0300

    fix

--- js/full/parse.js
+++ js/full/parse.js
@@ -1,14 +1,106 @@
-function parseHTML(blocs, blocShow) {
+$(document).ready(function() {
+    var $content = $('.section-content-js'),
+      $cod = $('.section-cod-js'),
+      $area = $('.area-js'),
+      $btnPanel = $('.btn-panel'),
+      $btnParse = 'btn-parse-js',
+      $btnClear = 'btn-clear-js',
+      disabledClass = 'disabled';
+
+  $('.filters-js')
+    .on('change', '.stylesheet_language-radio-js', function() {
+      $(this)
+        .parent()
+        .parent()
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+    });
+
+
+  $('.section-form-js')
+    .on('submit', function(event) {
+      event.preventDefault();
+      var $this = $(this),
+          $data = $this.serializeArray(),
+          $buttonSubmit = $this.find('.' + $btnParse),
+          $buttonReset = $this.find('.' + $btnClear),
+          $buttonRadio = $this.find('.stylesheet_language-radio-js'),
+          $stylesheet_language = $data[0].value,
+          $html = $data[1].value;
+
+      if ($html) {
+        $buttonSubmit
+          .attr('disabled', 'disabled');
+        $buttonRadio
+          .attr('disabled', 'disabled');
+        $buttonReset
+          .show();
+        $cod
+          .html($html);
+
+        var $info = $cod.find('*');
+
+        parseHTML($info, $content, $stylesheet_language);
+
+      } else {
+        $area
+          .addClass('has-error');
+        alert('Area is empty!');
+      }
+
+    });
+
+  $area
+    .on('change', function() {
+      var $this = $(this),
+          $thisVal = $this.val();
+
+      if ($thisVal == '') {
+        $('.' + $btnParse)
+          .removeAttr('disabled');
+        $('.stylesheet_language-radio-js')
+          .removeAttr('disabled');
+        $cod
+          .html('');
+        $content
+          .html('');
+        $('.' + $btnClear)
+          .hide();
+      } else {
+        $this
+          .removeClass('has-error');
+      }
+    });
+
+  $('.' + $btnClear)
+    .on('click', function() {
+      $(this)
+        .hide();
+      $('.' + $btnParse)
+        .removeAttr('disabled');
+      $('.stylesheet_language-radio-js')
+        .removeAttr('disabled');
+      $area
+        .val('');
+      $cod
+        .html('');
+      $content
+        .html('');
+    });
+});
+
+function parseHTML(blocs, blocShow, stylesheet_language) {
 
   var $element = blocs,
-      $arr = [];
+      $arr = [],
+      target = ' ';
 
   $element.each(function() {
 
     var $this = $(this),
-        $thisClass = $this.attr('class'),
-        target = ' ';
-
+        $thisClass = $this.attr('class');
+        
     if ($thisClass) { // Перевіряємо на наявнісь класу
 
       // Перевіряємо на наявнісь декількох класів у елемента
@@ -23,17 +115,31 @@ function parseHTML(blocs, blocShow) {
   });
 
   var $uniqueArr = unique($arr), // Видаляємо всі дублюючі класи
-      $uniqueArrLength = $uniqueArr.length;
-  
+      $noJsArr = nojsClass($uniqueArr),
+      // $sortArr = BubbleSort($noJsArr),
+      $mainArr = $noJsArr,
+      $mainArrLength = $mainArr.length;
+
+  // console.log($nnn);
 
   blocShow
       .append('<h2 class="title">The backbone of classes from HTML tags:</h2>');
 
-  for (var i = 0, iLength = $uniqueArrLength; i < iLength; i++) {
-    blocShow
-      .append('.' + $uniqueArr[i] + '{}<br>');
-  }
+  for (var i = 0, iLength = $mainArrLength; i < iLength; i++) {
+    var styleClass = $mainArr[i],
+        styleClassItem = '';
 
+    if (stylesheet_language == 'SASS') {
+      styleClassItem = '.' + styleClass + '<br>';
+    } else {
+      styleClassItem = '.' + styleClass + '{}<br>';
+    }
+
+
+
+    blocShow
+      .append(styleClassItem);
+  }
 
 }
 
@@ -50,4 +156,32 @@ function unique(arr) {
     }
 
   return result;
+}
+
+
+function nojsClass(arr) {
+  var result = [],
+      myRe = /-js/;
+
+  for (var i = 0; i < arr.length; i++) {
+    var str = arr[i];
+
+    if (!myRe.test(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+function BubbleSort(A) {
+    var n = A.length;
+    for (var i = 0; i < n-1; i++) {
+      for (var j = 0; j < n-1-i; j++) { 
+        if (A[j+1] < A[j]) { 
+          var t = A[j+1]; A[j+1] = A[j]; A[j] = t;
+        }
+      }
+    }
+    return A;
 }
--- js/init.js
+++ js/init.js
@@ -19,74 +19,7 @@ if (head.browser.ie && head.browser.version < 9) {
 head.js(
   paths.jqueryJS,
   paths.source + 'js/full/parse.js',
-  function() {
-
-    var $content = $('.section-content-js'),
-        $cod = $('.section-cod-js'),
-        $area = $('.area-js'),
-        $btnPanel = $('.btn-panel'),
-        $btnParse = 'btn-parse-js',
-        $btnClear = 'btn-clear-js',
-        disabledClass = 'disabled';
-
-    $btnPanel
-      .on('click', '.' + $btnParse, function() {
-        var $this = $(this),
-            $value = $area.val();
-
-        if ($value != '') {
-
-          if (!$this.hasClass(disabledClass)) {
-            $this
-              .addClass(disabledClass);
-
-            $btnPanel
-              .append('<span class="btn btn-fake ' + $btnClear + '">Clear</span>');
-
-            $cod
-              .html($value);
-
-            var $info = $cod.find('*');
-
-            parseHTML($info, $content);
-          }
-
-        } else {
-          alert('area is empty!');
-        }
-      });
-
-    $area
-      .on('change', function() {
-        var $this = $(this),
-            $thisVal = $this.val();
-
-        if ($thisVal == '') {
-          $('.' + $btnParse)
-            .removeClass(disabledClass);
-          $cod
-            .html('');
-          $content
-            .html('');
-          $('.' + $btnClear)
-            .remove();
-        }
-      });
-
-    $('.btn-panel')
-      .on('click', '.' + $btnClear, function() {
-        $(this)
-          .remove();
-        $('.' + $btnParse)
-          .removeClass(disabledClass);
-        $area
-          .val('');
-        $cod
-          .html('');
-        $content
-          .html('');
-      });
-  }
+  function() {}
 );
 
 if (head.browser.ie && head.browser.version < 10 || head.browser.opera) {
